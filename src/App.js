9ccd6ad2a707a19b143ff0432ccf73cc43f5878d
dennismacc <dennis.maccarthy@gmail.com>
2022-10-03 23:55:08 -0700

    add about and page files to components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Page from './components/Page';
 
 function App() {
+  const [pages] = useState([
+    { name: 'About Me' },
+    { name: 'Portfolio' },
+    { name: 'Contact' },
+    { name: 'Resume' },
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <Nav
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        ></Nav>
+      </Header>
+      <main>
+        <Page currentPage={currentPage}></Page>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
